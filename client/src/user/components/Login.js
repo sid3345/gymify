1eeb020aa93d2fea804fd509956f0a7b84217af6
Siddharth Sinha <sid3345@yahoo.in>
2021-05-07 22:56:01 +0530

    removed admin login

--- client/src/user/components/Login.js
+++ client/src/user/components/Login.js
@@ -10,10 +10,7 @@ function Login() {
 
     const signIn = e =>{
         e.preventDefault()
-        if (email== 'admin@admin.com' && password== 'admin'){
-                    history.push('/admin')
-                }
-        else{
+
             auth
                 .signInWithEmailAndPassword(email , password)
                 .then(auth => {
@@ -21,7 +18,6 @@ function Login() {
                     history.push('/')
                 })
                 .catch(error => alert(error.message))
-            }
     }
 
     const register = e =>{

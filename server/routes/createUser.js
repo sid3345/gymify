185961ed49eb635ce480cab6d0dc1a99d87f719d
Siddharth Sinha <sid3345@yahoo.in>
2021-05-09 14:19:09 +0530

    revert user DB changes

--- server/routes/createUser.js
+++ server/routes/createUser.js
@@ -7,7 +7,7 @@ router.route("/").post((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(req.body);
   console.log(req.body)
-  db.collection("users").doc(req.body.email).set({
+  db.collection("users").add({
     email : req.body.email,
     checked : req.body.check.checked
   })

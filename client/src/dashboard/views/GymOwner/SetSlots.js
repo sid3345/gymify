3aa686786e40615307ab3ce11601b0812344e6f5
Siddharth Sinha <sid3345@yahoo.in>
2021-05-23 18:35:32 +0530

    gym owner set slot done

--- client/src/dashboard/views/GymOwner/SetSlots.js
+++ client/src/dashboard/views/GymOwner/SetSlots.js
@@ -18,18 +18,20 @@ class CreateEvent extends Component {
 
     this.onChangeDate = this.onChangeDate.bind(this);
     this.onChangeTimezone = this.onChangeTimezone.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
     this.getSlots = this.getSlots.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.onGetSlot = this.onGetSlot.bind(this);
     this.onSlotSelect = this.onSlotSelect.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       date: new Date(),
       timezones: timezones,
       timezone: "",
-      duration: 0,
       slots: [],
+      slots_booked:[],
       buttons: [],
+      gymList:[],
+      all_slots:[]
     };
   }
   
@@ -38,8 +40,25 @@ class CreateEvent extends Component {
     this.setState({
       date: new Date(),
       timezone: "Asia/Kolkata",
-      duration: 0,
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+      
+    if(this.props.userState.user && this.state.gymList.length==0){
+
+      axios.get("http://localhost:5000/gymList/").then((res)=> {
+        //console.log('res: ', res);
+
+        for (var i in res.data){
+            if(res.data[i].email== this.props.userState.user.email){
+            this.setGymList(res.data[i])
+            break
+            }
+          }
+          //console.log('gymList: ', this.state.gymList);
+      });
+    }
   }
 
   onChangeTimezone(e) {
@@ -48,49 +67,65 @@ class CreateEvent extends Component {
     });
   }
 
-  onChangeDuration(e) {
-    this.setState({
-      duration: e.target.value,
-    });
-  }
-
   onChangeDate(date) {
     this.setState({
       date: date,
+      slots_booked:[]
+    })
+    if (this.state.gymList.slots)
+    this.state.gymList.slots.map((eachDate) =>  Number(Object.keys(eachDate)) == date.getDate() ?
+    this.setState({
+      slots_booked: eachDate[Object.keys(eachDate)]
+    })
+    :null)
+
+    this.onGetSlot()
+  }
+
+  setGymList(gymList) {
+    this.setState({
+      gymList: gymList,
+      all_slots: gymList.slots
     });
   }
 
   getSlots(availableSlots) {
     let refSlots = [];
+    //console.log('availableSlots: ', availableSlots);
+
     availableSlots.map((slot) => {
-      // console.log((moment.tz(slot , this.state.timezone)).date())
-      // console.log(this.state.date.getDate())
+       //console.log('slot1: ',new Date(slot))
+      let booked=0;
+
+      for (var i=0; i <  this.state.slots_booked.length; i++){
+        if (new Date(slot).getHours()== new Date(this.state.slots_booked[i]).getHours())
+          booked=1
+      }
+
       if(this.state.date.getDate() === (moment.tz(slot , this.state.timezone)).date()){
         
         if(new Date().getHours() < (moment.tz(slot , this.state.timezone)).hours()){
         
-          refSlots.push(moment.tz(slot, this.state.timezone));
+          refSlots.push({slot_available: moment.tz(slot, this.state.timezone), booked: booked});
         
           }
-
       }
       else{
-        refSlots.push(moment.tz(slot, this.state.timezone));
-
+        refSlots.push({slot_available: moment.tz(slot, this.state.timezone), booked: booked});
       }
 
-      
       return moment.tz(slot, this.state.timezone);
     });
+
     this.setState({
-      slots: refSlots,
+      slots: refSlots
     });
+    
     let tmp = [];
     this.state.slots.map((slot) => {
-      // console.log(new Date().getHours())
       let cur_hr = new Date().getHours()
-      let Hours = slot.hours();
-      let min = slot.minutes();
+      let Hours = slot.slot_available.hours();
+      let min = slot.slot_available.minutes();
       let _hrs = Hours;
       let _daynight = "AM";
       // if(cur_hr < Hours){
@@ -103,19 +138,17 @@ class CreateEvent extends Component {
         }
         let _min = min;
         if (min === 0) _min = "00";
-        tmp.push(`${_hrs}:${_min} ${_daynight}`);
+        tmp.push({tmp: `${_hrs}:${_min} ${_daynight}`, booked: slot.booked});
         return tmp;
       // }
       
     });
     this.setState({
-      buttons: tmp,
+      buttons: tmp
     });
   }
 
-  onSubmit(e) {
-    e.preventDefault();
-
+  onGetSlot() {
     const events = {
       reqDate: this.state.date.valueOf(),
       reqTimezone: this.state.timezone,
@@ -126,15 +159,30 @@ class CreateEvent extends Component {
     });
   }
 
+
   onSlotSelect(e) {
-    if(window.confirm("Are You Sure wanna continue?")){
+
       let refSlots = [];
       this.state.slots.map((slot) => {
-        refSlots.push(moment.tz(slot, "Asia/Kolkata"));
+        refSlots.push(moment.tz(slot.slot_available, "Asia/Kolkata"));
         return moment.tz(slot, "Asia/Kolkata");
       });
-      let index = this.state.buttons.indexOf(e.button);
+      e.button.booked=1;
+
+      //let index = this.state.buttons.tmp.indexOf(e.button.tmp);
+      let index=0
+
+      for (var j=0; j< this.state.buttons.length; j++){
+          if (this.state.buttons[j].tmp== e.button.tmp){
+            index= j;
+            break
+          }
+        }
+
       const selectedSlot = refSlots[index];
+      
+      //console.log('selectedSlot: ',selectedSlot);
+      
       let date = this.state.date,
         yr = date.getFullYear(),
         month = date.getMonth(),
@@ -146,18 +194,45 @@ class CreateEvent extends Component {
       let eventDateTime = moment
         .tz([yr, month, day, hr, min], "Asia/Kolkata")
         .format();
-      // console.log(eventDateTime);
+      
+      if(!(this.state.slots_booked.includes(eventDateTime))){
+      this.setState(prevState => ({
+          slots_booked: [...prevState.slots_booked, eventDateTime]
+        }))
+      }
+      
+        //console.log('this.state.slots_booked: ', this.state.slots_booked);
+    }
+  
+  onSubmit(){
+
+    if(window.confirm("Are you sure you want to continue?")){
+
+    let final_slots={}
+    final_slots[Number(this.state.date.getDate())]= this.state.slots_booked
+
+    let arr= this.state.all_slots
+
+    arr = arr.filter(item => Number(Object.keys(item)[0]) !== this.state.date.getDate())
+    arr.push(final_slots)
+
+    
+    this.setState({
+        all_slots: arr
+      })
 
       const eventParam = {
-        slots: eventDateTime,
+        slots: arr,
         email: this.props.userState.user.email,
       };
 
-    axios.post("http://localhost:5000/gym_register/", eventParam).then((res) => {
-      console.log(res.data);
-    });
+      //console.log('eventParam: ', eventParam);
     
-    } 
+    axios.post("http://localhost:5000/gym_register", eventParam).then((res) => {
+      console.log(res.data);
+      window.location.reload()
+      });
+    }
   }
 
   render() {
@@ -170,7 +245,6 @@ class CreateEvent extends Component {
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <label> Date:</label>
-                {console.log('moment().toDate(): ',moment().toDate())}
                 <div>
                   <DatePicker
                     selected={this.state.date}
@@ -179,42 +253,31 @@ class CreateEvent extends Component {
                   />
                 </div>
               </div>
-              
               <div className="form-group">
-                <label> Duration</label>
-                <input
-                  type="number"
-                  className="form-control"
-                  value={this.state.duration}
-                  onChange={this.onChangeDuration}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="submit"
-                  value="Set Slots"
-                  className="btn btn-primary"
-                />
-              </div>
+            <input
+              type="submit"
+              value="Submit"
+              className="btn btn-primary"
+            />
+          </div>
             </form>
           </div>
           <div className="col-12 col-md-6">
             {this.state.buttons.map((button) => {
               return (
                 <Button
-                  key = {button}
+                  key = {button.tmp}
                   style={{width : "120px" , height : "auto"}}
-                  color="primary"
+                  color={button.booked==1 ? 'success' : "primary"}
                   className="m-2"
                   onClick={() => {
                     this.onSlotSelect({ button });
                   }}
                 >
-                  {button}
+                  {button.tmp}
                 </Button>
               );
             })}
-    
           </div>
         </div>
       </div>

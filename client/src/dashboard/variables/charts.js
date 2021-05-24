80f4240d9778f4e0881f897dbddd94e9ef226e39
Siddharth Sinha <sid3345@yahoo.in>
2021-05-24 20:23:09 +0530

    Update charts.js

--- client/src/dashboard/variables/charts.js
+++ client/src/dashboard/variables/charts.js
@@ -18,7 +18,7 @@ var delays2 = 80,
 const dailySalesChart = {
   data: {
     labels: ["M", "T", "W", "T", "F", "S", "S"],
-    series: [[12, 17, 7, 17, 23, 18, 38]]
+    series: [[2, 7, 7, 6, 3, 2, 8]]
   },
   options: {
     lineSmooth: Chartist.Interpolation.cardinal({
@@ -85,7 +85,7 @@ const emailsSubscriptionChart = {
       "Nov",
       "Dec"
     ],
-    series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
+    series: [[15, 24, 32, 10, 32, 43, 36, 34, 18, 10, 22, 48]]
   },
   options: {
     axisX: {
@@ -137,7 +137,7 @@ const emailsSubscriptionChart = {
 const completedTasksChart = {
   data: {
     labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-    series: [[230, 750, 450, 300, 280, 240, 200, 190]]
+    series: [[10, 16, 15, 18, 10, 4, 8, 11]]
   },
   options: {
     lineSmooth: Chartist.Interpolation.cardinal({
